3cca4086c0349c3b0fb9c4b9eff3587c92e4675d
code <idulsa.emmannicholas@gmail.com>
2023-05-16 11:56:23 +0800

    added folders

---
+++ Data_Types.py
@@ -0,0 +1,14 @@
+#Data Types
+
+# Int
+813217231981
+
+#Float
+0.923113, 1.123123
+
+#String
+"jack"
+'ball'
+
+#Bool
+True, False
---
+++ Variables.py
@@ -0,0 +1,13 @@
+hello = "Hi"
+world = "World"
+print(hello, world)
+
+hi = "nic"
+yo = "hey"
+yo = hi
+hi = 'not'
+
+print(hi, yo)
+
+hello_world = 'hello nics'
+print(hello_world)
